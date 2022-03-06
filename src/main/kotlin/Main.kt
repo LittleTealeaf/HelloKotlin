2ae740aeb58ea1ab071dc0d12788692bf69c9bb6
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-05 21:54:28 -0500

    Implemented Fibonacci sequence

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,39 @@
-/**
- * Why I decided to do this in french, only god knows
- */
-fun main(args: Array<String>) {
-    println("Bonjour, je m'appelle Kotlin, et vous?");
-    val name = readLine()
-    println("Ooh, $name, c'est un beau nom!")
+fun main() {
+    println("Please select a prompt:")
+    println("1. Collatz Conjesture\n" +
+            "2. Fibonacci Sequence\n")
+
+    when(Integer.valueOf(readLine())) {
+        1 -> collatzConjecture()
+        2 -> fibonacciSequence()
+
+        else -> println("Whoops, try again next time");
+    }
+}
+
+fun collatzConjecture() {
+    println("Give me a number");
+    var i = Integer.valueOf(readLine());
+    var count = 0;
+    while(i > 1) {
+       i = if(i%2==0) i / 2 else i * 3 + 1
+        count++;
+    }
+
+    println("That took $count steps");
+}
+
+fun fibonacciSequence() {
+    println("What number do you want in the fibonacci sequence?")
+    var i = Integer.valueOf(readLine());
+
+    var a = 0
+    var b = 1
+    for(i in 2..i) {
+        b += a;
+        a = b - a;
+    }
+
+    print("That is $b")
+
 }
